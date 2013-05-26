77743296bf87bf4e7cda5ce34144e2df96b872b0
skaufman <sam@socialflow.com>
2013-05-26 15:15:15 -0400

    chrome compatibility

--- static/js/mr/studio.js
+++ static/js/mr/studio.js
@@ -1,3 +1,18 @@
-function Studio( $scope ) {
+var _num_circles = 6;
+function Studio($scope) {
+    var _circles = [];
+    var _leds = [];
+    for (var i = 0; i < _num_circles; i++) {
+        _circles.push({
+            class: 'del' + i
+        });
+        _leds.push({
+            class: 'led' + i
+        });
+    }
+    angular.extend($scope, {
+        circles: _circles,
+        leds: _leds
+    });
     return $scope;
 }
